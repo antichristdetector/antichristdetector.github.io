77f8dac96be1a28233085386eb439170549ec0f9
Guy Royse <guy@guyroyse.com>
2017-01-18 20:13:42 -0500

    m0ar stuff

--- detector.js
+++ detector.js
@@ -1,1 +1,12 @@
 var endpoint = 'https://0rac19uawj.execute-api.us-east-1.amazonaws.com/prod/detect';
+
+$(function() {
+
+  $('#detect').on('click', function() {
+
+    var name = $('#inputName').val();
+
+  });
+
+
+});
